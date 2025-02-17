c4d4788617373228977c67b647365c8784b50bfe
kingilsildor <tychostam@gmail.com>
2025-02-17 12:45:34 +0100

    update to boundary conditions of diffusion

--- modules/diffusion_equation.py
+++ modules/diffusion_equation.py
@@ -1,87 +1,75 @@
-from time import time
-
-import matplotlib.pyplot as plt
 import numpy as np
-import scipy.ndimage
+import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import scipy.ndimage as ndimage
 from numba import njit
 
+def init_grid(N: int, N_time_steps: int, init_condition: str) -> np.ndarray:
+    grid = np.zeros((N_time_steps, N, N)) 
+    assert grid.shape == (N_time_steps, N, N), "Invalid grid shape"
+    
+    assert init_condition in ["center", "random", "top", "bottom"], "Invalid initial condition"
+    if init_condition == "center":
+        grid[0, N//2, N//2] = 1
+    elif init_condition == "random":
+        grid[0] = np.random.rand(N, N)
+    elif init_condition == "top":
+        grid[0, 0, :], grid[0, -1, :] = 1, 0
+    elif init_condition == "bottom":
+        grid[0, 0, :], grid[0, -1, :] = 0, 1
 
-
-
-def init_grid(N):
-    grid = np.zeros((N, N))
-    grid[0, :] = 1
     return grid
 
-def create_empty_dict(length):
-    keys = [f"key_{i}" for i in np.arange(length)]
-    values = np.full(length, None)
-    empty_dict = dict(zip(keys, values))
-    assert len(empty_dict) == length
-    return empty_dict
-
-
-@njit()
-def compute_diffusion(grid, D, dt, dx, N):
-    diffusion_coefficient = ((D * dt / dx) ** 2)
-    new_grid = np.empty_like(grid)
-
-    for i in range(1, N - 1):
-        for j in range(1, N - 1):
-            new_grid[i, j] = grid[i, j] + diffusion_coefficient * (
-                grid[(i + 1) % N, j]
-                + grid[(i - 1) % N, j]
-                + grid[i, (j + 1)]
-                + grid[i, (j - 1)]
-                - 4 * grid[i, j]
+# @njit
+def diffuse(grid: np.ndarray, diffusion_coefficient: float) -> np.ndarray:
+    new_grid = np.copy(grid)
+    N = grid.shape[1]
+    for i in range(N):
+        for j in range(1, N-1):
+            new_grid[i, j] += diffusion_coefficient * (
+                grid[(i + 1) % N, j] +
+                grid[(i - 1) % N, j] +
+                grid[i, j + 1] +
+                grid[i, j - 1] - 4 * grid[i, j]
             )
+    new_grid[0, :], new_grid[-1, :] = 1, 0
     return new_grid
 
-
-def update_grid(grid, D, dt, dx, N_TIME_STEPS, smooth=True):
-    N = grid.shape[0]
-    grid_dict = create_empty_dict(N_TIME_STEPS)
-
-    for i in grid_dict.keys():
-        grid = compute_diffusion(grid, D, dt, dx, N)
-
-        grid[0, :], grid[-1, :] = 1, 0
+def calc_diffusion(N: int, N_time_steps: int, D: float, dx: float, dt: float, init_condition: str = "top", smooth: bool = True) -> np.ndarray:
+    diffusion_coefficient = ((D * dt / dx) ** 2)
+    grid = init_grid(N, N_time_steps, init_condition)
+    for i in range(1, N_time_steps):
+        grid[i] = diffuse(grid[i-1], diffusion_coefficient)
         if smooth:
-            grid[:] = scipy.ndimage.gaussian_filter(grid, sigma=0.5)
+            grid[:] = ndimage.gaussian_filter(grid, sigma=0.5)
+    return grid
 
-        grid_dict[i] = grid.copy()
+def animate_diffusion(grid : np.ndarray) -> None:
+    steps = grid.shape[0]
 
-    return grid_dict
-
-def calculate_diffusion(N, D, dt, dx, N_TIME_STEPS):
-    grid = init_grid(N)
-    data = update_grid(grid, D, dt, dx, N_TIME_STEPS, smooth=True)
-
-    np.savez("data/diffusion_grid.npz", **data)
-
-def animate_diffusion(data=None):
-    data = np.load("data/diffusion_grid.npz")
     fig, ax = plt.subplots()
+    cmap = plt.get_cmap('inferno')
+    im = ax.imshow(grid[0], cmap=cmap, interpolation='nearest')
+    plt.colorbar(im)
 
-    cax = ax.imshow(data['key_0'], cmap='inferno', interpolation='nearest')
-    
-    def update(frame):
-        cax.set_array(data[f'key_{frame}'])
-        ax.set_title(f"Time step {frame + 1}")
-        return cax,
+    def animate(frame):
+        im.set_array(grid[frame])
+        ax.set_title(f"Time step {frame}")
 
-    N_frames = len(data)
-    ani = FuncAnimation(fig, update, frames=N_frames, interval=50, blit=True)
-    ani.save("results/diffusion_simulation.mp4", writer="ffmpeg", fps=60)
+    ani = FuncAnimation(fig, animate, frames=steps, interval=50)
+    ani.save("heat_diffusion.mp4", writer="ffmpeg", fps=30)
 
+def calc_coverage(grid: np.ndarray) -> float:
+    return np.mean(grid)
 
-N = 200
-D = 1
-T = 1
-N_TIME_STEPS = 2000
-dx = 1 / N
-dt = T / N_TIME_STEPS
+def main():
+    N = 50
+    D = 1
+    dx = 1/N
+    dt = 0.01
+    steps = 2000
+    grid = calc_diffusion(N, steps, D, dx, dt)
+    animate_diffusion(grid)
 
-calculate_diffusion(N, D, dt, dx, N_TIME_STEPS)
-animate_diffusion()
+if __name__ == "__main__":
+    main()
